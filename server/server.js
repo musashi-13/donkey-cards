f74751c6ad1ae51f8697228f3ad3a7c016cfbf69
Karan Hathwar <karanhathwar@gmail.com>
2024-08-29 15:22:26 +0530

    Fixing even now

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const io = require('socket.io')(3000, {
     cors: {
         origin: 'http://localhost:3001',
@@ -11,65 +10,65 @@ const { determineStartingPlayer, playRound, checkEndCondition } = require('./gam
 const rooms = {};
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    console.log('a user connected');
 
     socket.on('joinRoom', async (roomName, userName) => {
-        console.log(`${userName} has joined ${roomName}`);
+        console.log('joinRoom: ' + roomName + ' ' + userName);
+        socket.username = userName;
         if (!rooms[roomName]) {
-            rooms[roomName] = { players: [], deckId: null };
+            rooms[roomName] = { players: [], deckId: null, hands: {}, currentSuit: null };
         }
-        rooms[roomName].players.push({ id: socket.id, username: userName, hand: []});
+        rooms[roomName].players.push(userName);
         socket.join(roomName);
         io.to(roomName).emit('updatePlayers', rooms[roomName].players);
     });
 
     socket.on('startGame', async (roomName) => {
-        console.log(`Game started for ${roomName}`);
-        if (rooms[roomName] && rooms[roomName].players.length > 0) {
+        console.log("game started for " + roomName);
+        const room = rooms[roomName];
+        if (room && room.players.length > 0) {
             // Create a new deck if not already created
-            if (!rooms[roomName].deckId) {
-                console.log(`Getting deckId for ${roomName}`);
+            if (!room.deckId) {
                 const deckResponse = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-                rooms[roomName].deckId = deckResponse.data.deck_id;
-            } else{
-                console.log(`Reshuffling cards for ${roomName}`);
-                const deckResponse = await axios.get(`https://www.deckofcardsapi.com/api/deck/${deckId}/return/`);
-                rooms[roomName].deckId = deckResponse.data.deck_id;
+                room.deckId = deckResponse.data.deck_id;
             }
-            const deckId = rooms[roomName].deckId;
-            const players = rooms[roomName].players;
-            totalPlayers = totalPlayers > 3 ? totalPlayers : 4
-            const totalPlayers = players.length;  // Use the actual number of players
-            const cardsPerPlayer = Math.floor(52 / totalPlayers);
-            const extraCards = 52 % totalPlayers;
+            const deckId = room.deckId;
+            const players = room.players;
+            const cardsPerPlayer = Math.floor(52 / players.length);
+            const extraCards = 52 % players.length;
 
-            for (let i = 0; i < totalPlayers; i++) {
-                // Calculate the number of cards for the current player
+            for (let i = 0; i < players.length; i++) {
                 const numCards = i < extraCards ? cardsPerPlayer + 1 : cardsPerPlayer;
-
-                // Draw the cards for the current player
-                const drawResponse = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${numCards}`);
-                players[i].hand = drawResponse.data.cards;  // Assign hand to the player
+                const drawResponse = await axios.get("https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${numCards}");
+                room.hands[players[i]] = drawResponse.data.cards;
             }
 
-            // Send cards to each player
-            for (const player of players) {
-                const playerSocketId = player.id;
-                console.log(`Sending cards to player: ${player.username}, Socket ID: ${playerSocketId}`);
-                if (playerSocketId) {
-                    io.to(playerSocketId).emit('dealCards', player.hand);  // Send hand to the player
-                } else {
-                    console.error(`Player ${player.username} not found in socket connections`);
-                }
+            const startingPlayer = determineStartingPlayer(players, room.hands);
+            if (startingPlayer) {
+                room.currentSuit = 'SPADES'; // Start with Spades
+                io.to(roomName).emit('roundStart', startingPlayer, room.currentSuit);
             }
-        } else {
-            console.log("There are no players in the room");
+        }
+    });
+
+    socket.on('playCard', (roomName, player, card) => {
+        const room = rooms[roomName];
+        const players = room.players;
+        if (room && players.includes(player)) {
+            const nextPlayer = playRound(room, room.currentSuit, players);
+            const loser = checkEndCondition(room);
+            if (loser) {
+                io.to(roomName).emit('gameEnd', loser);
+            } else {
+                room.currentSuit = card.suit; // Set the current suit for the next round
+                io.to(roomName).emit('roundStart', nextPlayer, room.currentSuit);
+            }
         }
     });
 
     socket.on('disconnect', () => {
-        console.log('User disconnected');
+        console.log('user disconnected');
     });
 });
 
-console.log('Server-side code running');
+console.log('Server-side code running');
